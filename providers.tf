e8a48dc7c074897962882c9579087efc81249fe9
shan5a6 <shahan.aix@gmail.com>
2024-02-27 22:30:31 +0400

    add backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.9.0"
     }
   }
+    backend "azurerm" {
+        resource_group_name  = "b3-rg1" # Resource Group Name
+        storage_account_name = "dvsterrastate" # Storage Account Name
+        container_name       = "dvsterrastate " # Container/Blob Name
+        key                  = "terraform.tfstate" # State File Name
+    }
+
 }
 
 provider "azurerm" {
