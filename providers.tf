6bda5fca9ac32602d5fd06561021da2eeadd508d
shan5a6 <shahan.aix@gmail.com>
2024-02-28 20:08:56 +0400

    testing ci process

--- providers.tf
+++ providers.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.9.0"
     }
   }
-    backend "azurerm" {
-        resource_group_name  = "b3-rg1"
-        storage_account_name = "dvsterrastate"
-        container_name       = "stateconfiguration"
-        key                  = "terraform.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "b3-rg1"
+    storage_account_name = "dvsterrastate"
+    container_name       = "stateconfiguration"
+    key                  = "terraform.tfstate"
+  }
 
 }
 
 provider "azurerm" {
   # Configuration options
-        features {}
+  features {}
 
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "application_name" {}
-variable "location" {}
-variable "java_cidr" {
-    type    =   list(string)
+variable "application_name" {}
+variable "location" {}
+variable "java_cidr" {
+  type = list(string)
 }
-variable "java_sub_cidr" {
-type    =   list(string)
+variable "java_sub_cidr" {
+  type = list(string)
 }
-variable "vm_size" {}
-variable "vm_username" {}
-variable "vm_pass" {}
+variable "vm_size" {}
+variable "vm_username" {}
+variable "vm_pass" {}
